4daccdf20715c0b816b663eca53c0053f8cf14a1
cutemonster <ghostshow@yandex.ru>
2020-02-23 01:19:25 +0200

    little changes in main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import sys
 from Classes import Connection as classes
 if __name__ == '__main__':
-    with open(r"../config.json", "r+") as file:
+    with open(r"Classes/configs/config.txt", "r+") as file:
+    # with open(r"../config.json", "r+") as file:
         # creating connection to the database
         connection = classes.Connection(file)
 
