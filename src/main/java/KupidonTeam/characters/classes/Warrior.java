9bf7b02337c282bec3b43dce950fdd36c641f4fe
springtasks <lolitergames@gmail.com>
2020-04-25 23:06:39 +0300

    --Warrior added list of Skills

--- src/main/java/KupidonTeam/characters/classes/Warrior.java
+++ src/main/java/KupidonTeam/characters/classes/Warrior.java
@@ -1,10 +1,15 @@
 package KupidonTeam.characters.classes;
 
-import KupidonTeam.enums.Direction;
+import KupidonTeam.characters.classes.skills.behavior.Skill;
+import lombok.Data;
 
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class Warrior extends Entity {
+    private List<Skill> skills;
+
     public Warrior() {
         setHp(100);
         setStrength(10);
@@ -16,10 +21,11 @@ public class Warrior extends Entity {
         setIntelligence(11);
         setSpeed(11);
         setWisdom(10);
+        skills = new LinkedList<>();
     }
 
     public Warrior(int hp, int strength, int armorClass, int chance, int charisma, int constitution, int dexterity,
-                   int intelligence, int speed, int wisdom) {
+                   int intelligence, int speed, int wisdom, List<Skill> skills) {
         setHp(hp);
         setStrength(strength);
         setArmorClass(armorClass);
@@ -30,8 +36,14 @@ public class Warrior extends Entity {
         setIntelligence(intelligence);
         setSpeed(speed);
         setWisdom(wisdom);
+        setSkills(skills);
     }
 
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
 
-
+    public List<Skill> getSkills() {
+        return skills;
+    }
 }
